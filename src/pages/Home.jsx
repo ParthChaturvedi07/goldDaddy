b67f3de5bf6c89ed87d395f447c0667aa0f43be6
Parth Chaturvedi <parthchaturvedi0204@gmail.com>
2025-06-26 01:26:47 +0530

    landing page updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,21 @@ import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import OrangeCoin from "../assets/images/CoinFullOrange.png";
 import TealCoin from "../assets/images/CoinFullBlue.png";
-import gsap from "gsap";
+import GoldBar from "../assets/images/GoldBar.svg";
+import Cash from "../assets/images/cash1.svg";
+import Jail from "../assets/images/Jail.svg";
+import watch from "../assets/images/watches.svg";
 import { useGSAP } from "@gsap/react";
 import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+
+import gsap from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(SplitText);
 
 export const Home = () => {
   const OrangeCoinRef = useRef(null);
@@ -48,7 +60,6 @@ export const Home = () => {
       .from(
         ".explore",
         {
-        
           opacity: 0,
           duration: 0.8,
           ease: "power4.inOut",
@@ -71,11 +82,228 @@ export const Home = () => {
       ease: "linear",
       transformOrigin: "50% 50%",
     });
+
+    let split = SplitText.create(".text", {
+      type: "chars",
+    });
+
+    const tl2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".service",
+        scroller: "body",
+        start: "top 80%",
+        end: "bottom 20%",
+        // markers: true,
+        toggleActions: "play none none reverse",
+      },
+    });
+
+    tl2.from(split.chars, {
+      opacity: 0,
+      y: 70,
+      stagger: 0.03,
+      duration: 0.6,
+      ease: "back.out(1.7)",
+    });
+
+    let splited = SplitText.create(".buy", {
+      type: "chars",
+    });
+
+    const tl3 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".buy-sell-gold",
+        scroller: "body",
+        start: "top 68%",
+        end: "bottom 20%",
+        // markers: true,
+        toggleActions: "play none none reverse",
+      },
+    });
+
+    tl3
+      .from(
+        splited.chars,
+        {
+          opacity: 0,
+          y: 70,
+          stagger: 0.03,
+          duration: 0.6,
+          ease: "back.out(1.7)",
+        },
+        "reveal"
+      )
+      .from(
+        ".goldbar",
+        {
+          opacity: 0,
+          y: 150,
+          duration: 0.6,
+          ease: "back.out(1.7)",
+        },
+        "reveal"
+      )
+      .from(".service hr", {
+        clipPath: "inset(0 0% 0 0)",
+        duration: 1,
+        ease: "power2.out",
+      });
+
+    let spliting = SplitText.create(".instant", {
+      type: "chars",
+    });
+
+    const tl4 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".cash-container",
+        scroller: "body",
+        start: "top 68%",
+        end: "bottom 20%",
+        // markers: true,
+        toggleActions: "play none none reverse",
+      },
+    });
+
+    tl4
+      .from(
+        spliting.chars,
+        {
+          opacity: 0,
+          y: 70,
+          stagger: 0.03,
+          duration: 0.6,
+          ease: "back.out(1.7)",
+        },
+        "reveal"
+      )
+      .from(
+        ".cash",
+        {
+          opacity: 0,
+          y: 150,
+          duration: 0.6,
+          ease: "back.out(1.7)",
+        },
+        "reveal"
+      );
+
+    let slipt = SplitText.create(".release-text", {
+      type: "chars",
+    });
+
+    const tl5 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".release",
+        scroller: "body",
+        start: "top 68%",
+        end: "bottom 20%",
+        // markers: true,
+        toggleActions: "play none none reverse",
+      },
+    });
+
+    tl5
+      .from(
+        slipt.chars,
+        {
+          opacity: 0,
+          y: 70,
+          stagger: 0.03,
+          duration: 0.6,
+          ease: "back.out(1.7)",
+        },
+        "reveal"
+      )
+      .from(
+        ".jail",
+        {
+          opacity: 0,
+          y: 150,
+          duration: 0.6,
+          ease: "back.out(1.7)",
+        },
+        "reveal"
+      );
+    let slipted = SplitText.create(".watch-text", {
+      type: "chars",
+    });
+
+    const tl6 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".watch-container",
+        scroller: "body",
+        start: "top 60%",
+        end: "bottom 20%",
+        // markers: true,
+        toggleActions: "play none none reverse",
+      },
+    });
+
+    tl6
+      .from(
+        slipted.chars,
+        {
+          opacity: 0,
+          y: 70,
+          stagger: 0.03,
+          duration: 0.6,
+          ease: "back.out(1.7)",
+        },
+        "reveal"
+      )
+      .from(
+        ".watch",
+        {
+          opacity: 0,
+          y: 150,
+          duration: 0.6,
+          ease: "back.out(1.7)",
+        },
+        "reveal"
+      );
+
+    const tl7 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".watch-container",
+        scroller: "body",
+        start: "top 20%",
+        end: "top 0",
+        scrub: 1.2,
+        markers: true,
+        toggleActions: "play none none reverse",
+      },
+    });
+
+    tl7.from(".footer-text", {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      ease: "power3.out",
+    });
+
+    // Handle resize and cleanup
+    const handleResize = () => {
+      ScrollTrigger.refresh();
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      // Kill all timelines and scroll triggers
+      tl.kill();
+      tl2.kill();
+      tl3.kill();
+      tl4.kill();
+      tl5.kill();
+      tl6.kill();
+      tl7.kill();
+      ScrollTrigger.getAll().forEach((st) => st.kill());
+    };
   }, []);
 
   return (
     <>
-      <div className="min-h-screen w-[100%] flex items-center justify-center bg-gradient-to-r from-orange-100 via-[#E1DDD7] to-teal-100 relative overflow-hidden">
+      <div className="min-h-screen w-[100%] pt-16 flex items-center justify-center flex-col bg-gradient-to-r from-orange-100 via-[#E1DDD7] to-teal-100 relative overflow-hidden">
         <div className=" h-screen sm:h-full container mx-auto px-4 py-16 md:py-24 relative z-10 flex flex-col items-center justify-center gap-8">
           {/* Hero Section */}
           <div className="container mx-auto px-4 py-16 md:py-24 flex flex-col items-center justify-center gap-5 text-center">
@@ -121,6 +349,123 @@ export const Home = () => {
             />
           </div>
         </div>
+
+        <div className="service container w-[100%] px-4 py-16">
+          <h2 className="text text-4xl sm:text-5xl text-[#525252] font-[Poi-medium] text-center mt-24 sm:mt-12 mb-16">
+            OUR SERVICES
+          </h2>
+
+          <div className="buy-sell-gold w-full max-w-5xl mx-auto">
+            <div className="flex flex-col md:flex-row items-center justify-between gap-8">
+              <div className="flex flex-col items-center md:items-start overflow-hidden">
+                <h3 className="buy text-5xl md:text-7xl font-[Poi-medium] mb-1">
+                  We Buy
+                </h3>
+                <h3 className="buy text-5xl md:text-7xl font-[Poi-medium] mb-1">
+                  &amp; Sell
+                </h3>
+                <span className="buy text-5xl md:text-7xl font-[Poi-medium] text-[#B87100]">
+                  Gold
+                </span>
+              </div>
+              <div className="flex justify-center">
+                <img
+                  src={GoldBar}
+                  alt="Gold Bar"
+                  className="goldbar w-48 h-48 md:w-80 md:h-80 sm:w-56 sm:h-56 object-contain"
+                />
+              </div>
+            </div>
+          </div>
+          <hr className="hr mx-auto max-w-3xl mt-12" />
+        </div>
+
+        <div className="cash-container container w-[100%] px-4">
+          <div className="w-full max-w-5xl mx-auto">
+            <div className="flex flex-col md:flex-row items-center justify-between gap-8 ">
+              <div className="flex justify-center">
+                <img
+                  src={Cash}
+                  alt=""
+                  className="cash w-48 h-48 md:w-80 md:h-80 sm:w-56 sm:h-56 object-contain"
+                />
+              </div>
+              <div className="flex flex-col items-center md:items-end overflow-hidden">
+                <h3 className="instant text-5xl md:text-7xl font-[Poi-medium] mb-1">
+                  Get Instant
+                </h3>
+                <span className="instant text-5xl md:text-7xl font-[Poi-medium] text-[#007537]">
+                  Cash
+                </span>
+              </div>
+            </div>
+          </div>
+          <hr className="hr mx-auto max-w-3xl mt-12" />
+        </div>
+
+        <div className="release container w-[100%] px-4 py-16">
+          <div className="w-full max-w-5xl mx-auto">
+            <div className="flex flex-col md:flex-row items-center justify-between gap-8">
+              <div className="flex flex-col items-center md:items-start overflow-hidden">
+                <h3 className="release-text text-5xl md:text-7xl font-[Poi-medium] mb-1">
+                  We Release
+                </h3>
+                <h3 className="release-text text-5xl md:text-7xl font-[Poi-medium] mb-1">
+                  and Buy
+                </h3>
+                <span className="release-text text-5xl md:text-7xl font-[Poi-medium] text-[#B87100]">
+                  Pledged Gold
+                </span>
+              </div>
+              <div className="jail flex justify-center">
+                <img
+                  src={Jail}
+                  alt="Gold Bar"
+                  className="w-48 h-48 md:w-80 md:h-80 sm:w-56 sm:h-56 object-contain"
+                />
+              </div>
+            </div>
+          </div>
+          <hr className="hr mx-auto max-w-3xl mt-12" />
+        </div>
+
+        <div className="watch-container w-full max-w-5xl mx-auto">
+          <div className="flex flex-col items-center justify-between gap-8">
+            <div className="pt-2 flex flex-col items-center overflow-hidden">
+              <h3 className="watch-text text-4xl md:text-6xl text-center font-[Poi-regular] mb-1">
+                We Buy & Sell Premium Watches
+              </h3>
+            </div>
+            <div className="flex justify-center">
+              <img
+                src={watch}
+                alt="Gold Bar"
+                className="watch w-48 h-48 md:w-80 md:h-80 sm:w-56 sm:h-56 object-contain"
+              />
+            </div>
+            <div className="flex flex-col items-center">
+              <h4 className="footer-text text-2xl md:text-3xl text-center font-[Poi-light] mb-1">
+                Sell your premium timepieces quickly and confidently.
+              </h4>
+            </div>
+
+            <Link
+              to="/gold-coins"
+              className="mb-5 explore cursor-pointer bg-black px-12 py-3 rounded-full border-[1px] border-slate-500 text-white font-[Poi-extraLight] group"
+            >
+              <div className="relative overflow-hidden">
+                <p className="group-hover:-translate-y-7 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]">
+                  ENQUIRE NOW
+                </p>
+                <p className="absolute top-7 left-0 group-hover:top-0 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]">
+                  ENQUIRE NOW
+                </p>
+              </div>
+            </Link>
+          </div>
+        </div>
+
+        <Footer />
       </div>
     </>
   );
